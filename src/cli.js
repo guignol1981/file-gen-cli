04724ac4d2519ecada6614b8b789aa97b2ace781
vincent guillemette <vincent.guillemette.1@ulaval.ca>
2019-03-30 16:10:12 -0400

    * config guard

--- src/cli.js
+++ src/cli.js
@@ -2,12 +2,16 @@ const inquirer = require('inquirer');
 
 module.exports = class CLI {
     constructor(config) {
-        if (!config || !Array.isArray(config.entityConfigs) || config.entityConfigs.length === 0) {
-            throw Error('there is a problem with your configuration, please refer to documentation');
-        }
+        this.configGuard();
 
         this.name = config.cliName || 'file-gen-cli';
         this.entityConfigs = config.entityConfigs;
+    }
+
+    configGuard(config) {
+        if (!config || !Array.isArray(config.entityConfigs) || config.entityConfigs.length === 0) {
+            throw Error('there is a problem with your configuration, please refer to documentation');
+        }
     }
 
     queryEntity() {
