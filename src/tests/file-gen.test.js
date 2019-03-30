ec597847db7bb88db94550efea663c12af4ea5ea
vincent guillemette <vincent.guillemette.1@ulaval.ca>
2019-03-30 16:23:59 -0400

    * config guard * tests

--- src/tests/file-gen.test.js
+++ src/tests/file-gen.test.js
@@ -1,1 +1,15 @@
-test('to do', () => expect(true).toBe(true));
+const FileGen = require('../file-gen');
+
+describe('when we construct a new file gen', () => {
+    it('should be an error if there is no config passed to the file gen', () => {
+        const test = () => new FileGen();
+
+        expect(test).toThrowError();
+    });
+
+    it('should be an error if there is no entities configured', () => {
+        let test = () => new FileGen({});
+
+        expect(test).toThrowError();
+    });
+});
