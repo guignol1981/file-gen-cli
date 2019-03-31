8c1b835a383fd8bb597613bf5b86b9ede368513a
Vincent Guillemette <vincentguillemette1981@gmail.com>
2019-03-31 17:53:56 -0400

    * test file generation

--- src/tests/file-gen.test.js
+++ src/tests/file-gen.test.js
@@ -1,4 +1,12 @@
+const fs = require('fs');
 const FileGen = require('../file-gen');
+const changeCase = require('change-case');
+const config = require('./gencli.json');
+const rimraf = require('rimraf');
+
+jest.mock('change-case');
+
+changeCase.kebab.mockImplementation(() => 'test');
 
 describe('when we construct a new file gen', () => {
     it('should be an error if there is no config passed to the file gen', () => {
@@ -32,5 +40,27 @@ describe('when we construct a new file gen', () => {
         });
 
         expect(test).toThrowError();
+    });
+
+});
+
+describe('when we generate a new entity instance', () => {
+    let fileGen;
+
+    beforeEach(() => {
+        fileGen = new FileGen(config);
+        fileGen.generate(config.entityConfigs[0], { singular: 'test', plural: 'tests' });
+    });
+
+    it('should create folder if it does not exist', () => {
+        expect(fs.existsSync('src/tests/test-entity/test')).toBe(true);
+    });
+
+    it('should create file', () => {
+        expect(fs.existsSync('src/tests/test-entity/test/test-test.ts')).toBe(true);
+    });
+
+    afterEach(() => {
+        rimraf.sync('src/tests/test-entity/test');
     });
 });
