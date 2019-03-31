8fa9bd124a2b0e201418c1da36ce64e1c9a594dc
Vincent Guillemette <vincentguillemette1981@gmail.com>
2019-03-31 18:23:11 -0400

    * keyword test

--- src/tests/file-gen.test.js
+++ src/tests/file-gen.test.js
@@ -6,7 +6,10 @@ const rimraf = require('rimraf');
 
 jest.mock('change-case');
 
+changeCase.pascal.mockImplementation(() => 'test');
+changeCase.camel.mockImplementation(() => 'test');
 changeCase.kebab.mockImplementation(() => 'test');
+changeCase.constant.mockImplementation(() => 'test');
 
 describe('when we construct a new file gen', () => {
     it('should be an error if there is no config passed to the file gen', () => {
@@ -47,6 +50,11 @@ describe('when we generate a new entity instance', () => {
     let fileGen;
 
     beforeEach(() => {
+        changeCase.camel.mockClear();
+        changeCase.pascal.mockClear();
+        changeCase.kebab.mockClear();
+        changeCase.constant.mockClear();
+
         fileGen = new FileGen(config);
         fileGen.generate(config.entityConfigs[0], { singular: 'test', plural: 'tests' });
     });
@@ -57,6 +65,25 @@ describe('when we generate a new entity instance', () => {
 
     it('should create file', () => {
         expect(fs.existsSync('src/tests/test-entity/test/test-test.ts')).toBe(true);
+    });
+
+    it('it should transform all template keywords', () => {
+        expect(changeCase.pascal).toBeCalledWith('test');
+        expect(changeCase.camel).toBeCalledWith('test');
+        expect(changeCase.kebab).toBeCalledWith('test');
+        expect(changeCase.constant).toBeCalledWith('test');
+        expect(changeCase.pascal).toBeCalledWith('tests');
+        expect(changeCase.camel).toBeCalledWith('tests');
+        expect(changeCase.kebab).toBeCalledWith('tests');
+        expect(changeCase.constant).toBeCalledWith('tests');
+        expect(changeCase.pascal).toBeCalledWith('test test');
+        expect(changeCase.camel).toBeCalledWith('test test');
+        expect(changeCase.kebab).toBeCalledWith('test test');
+        expect(changeCase.constant).toBeCalledWith('test test');
+        expect(changeCase.pascal).toBeCalledWith('test tests');
+        expect(changeCase.camel).toBeCalledWith('test tests');
+        expect(changeCase.kebab).toBeCalledWith('test tests');
+        expect(changeCase.constant).toBeCalledWith('test tests');
     });
 
     afterEach(() => {
