ecd50a4e83b801b72bf11069331f33630dfa7eda
Vincent Guillemette <vincentguillemette1981@gmail.com>
2019-04-13 17:01:13 -0400

    * change path

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const figlet = require('figlet');
 const chalk = require('chalk');
-const config = require(process.cwd() + '/example/gencli.json');
+const config = require(process.cwd() + '/gencli.json');
 const CLI = require('./src/cli');
 const FileGen = require('./src/file-gen');
 
