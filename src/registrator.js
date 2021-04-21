e2df56fd39755ea285275bd0b8d4f77a199f1d18
vincent <vincent.guillemette@dti.ulaval.ca>
2021-04-21 11:38:23 -0400

    filter tempalte

--- src/registrator.js
+++ src/registrator.js
@@ -13,8 +13,6 @@ module.exports = async (config, endpoint) => {
         },
     }).then((res) => res.json());
 
-    console.log(res.msg);
-
     if (!res.ok) {
         return;
     }
@@ -22,38 +20,44 @@ module.exports = async (config, endpoint) => {
     await Promise.all(
         config.entityConfigs.map(async (ec) => {
             await Promise.all(
-                ec.fileConfigs.map(async (fc) => {
-                    if (!fs.existsSync(path.join(process.cwd(), fc.template))) {
-                        throw `couldn't find ${fc.template} template, add the template in this folder and try again!`;
-                    }
-
-                    const formData = new FormData();
-
-                    formData.append(
-                        'file',
-                        fs.createReadStream(
-                            path.join(process.cwd(), fc.template)
-                        ),
-                        {
-                            filename: fc.template,
+                ec.fileConfigs
+                    .filter((fc) => fc.template)
+                    .map(async (fc) => {
+                        if (
+                            !fs.existsSync(
+                                path.join(process.cwd(), fc.template)
+                            )
+                        ) {
+                            throw `couldn't find ${fc.template} template, add the template in this folder and try again!`;
                         }
-                    );
-                    const res = await fetch(
-                        `${endpoint}/configs/${config.cliName}/files`,
-                        {
-                            method: 'POST',
-                            headers: {
-                                'Authorization': `Bearer ${config.token}`,
-                                'Content-Type':
-                                    'multipart/form-data; boundary=' +
-                                    formData.getBoundary(),
-                            },
-                            body: formData,
-                        }
-                    ).then((res) => res.json());
 
-                    console.log(res.msg);
-                })
+                        const formData = new FormData();
+
+                        formData.append(
+                            'file',
+                            fs.createReadStream(
+                                path.join(process.cwd(), fc.template)
+                            ),
+                            {
+                                filename: fc.template,
+                            }
+                        );
+                        const res = await fetch(
+                            `${endpoint}/configs/${config.cliName}/files`,
+                            {
+                                method: 'POST',
+                                headers: {
+                                    'Authorization': `Bearer ${config.token}`,
+                                    'Content-Type':
+                                        'multipart/form-data; boundary=' +
+                                        formData.getBoundary(),
+                                },
+                                body: formData,
+                            }
+                        ).then((res) => res.json());
+
+                        console.log(res.msg);
+                    })
             );
         })
     );
